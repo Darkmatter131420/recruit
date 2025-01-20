8ebac73e71bb8820b283aa919d31adb1d4750ea9
darkmatter <2781307050@qq.com>
2025-01-20 19:14:09 +0800

    2025/1/20

---
+++ Recruit-Springboot/recruit-auth/src/main/java/com/recruit/auth/RecruitAuthApplication.java
@@ -0,0 +1,13 @@
+package com.recruit.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecruitAuthApplication.class, args);
+    }
+
+}

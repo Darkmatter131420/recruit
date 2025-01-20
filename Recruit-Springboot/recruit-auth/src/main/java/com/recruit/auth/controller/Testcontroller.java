4105743c5a4e0ddce61b1a36184bd08788e0d6ca
darkmatter <2781307050@qq.com>
2025-01-20 20:19:42 +0800

    2025/1/20

---
+++ Recruit-Springboot/recruit-auth/src/main/java/com/recruit/auth/controller/Testcontroller.java
@@ -0,0 +1,19 @@
+package com.recruit.auth.controller;
+
+import com.recruit.framework.common.response.Response;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author DarkMatter
+ * @date 2025年1月20日20点11分
+ * @version 1.0.0
+ * @description: TODO
+ */
+@RestController
+public class Testcontroller {
+    @GetMapping("/test")
+    public Response<String> test() {
+        return Response.success("Hello World!");
+    }
+}
